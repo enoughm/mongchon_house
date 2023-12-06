430f287c3a69246aac9638ad1b6719a85e7f3ad8
khj <vrmorm@gmai.com>
2023-12-06 21:25:29 +0900

    wall 임시
    ㅇㅇ

--- Assets/Scripts/_Managers/GameManager.cs
+++ Assets/Scripts/_Managers/GameManager.cs
@@ -54,18 +54,9 @@ public class GameManager : MonoBehaviour
     
     private void Awake()
     {
-        Debug.Log("displays connected: " + Display.displays.Length);
-        // Display.displays[0] is the primary, default display and is always ON.
-        // Check if additional displays are available and activate each.
-        if (Display.displays.Length > 1)
-            Display.displays[1].Activate();
-        if (Display.displays.Length > 2)
-            Display.displays[2].Activate();
-        
         //find tag
-        _floorUrgTouchDetector = GameObject.FindGameObjectWithTag("FloorUrgTouchDetector").GetComponent<UrgTouchDetector>();
+        //_floorUrgTouchDetector = GameObject.FindGameObjectWithTag("FloorUrgTouchDetector").GetComponent<UrgTouchDetector>();
         _wallUrgTouchDetector = GameObject.FindGameObjectWithTag("WallUrgTouchDetector").GetComponent<UrgTouchDetector>();
-        
     }
 
     private void OnEnable()
@@ -95,7 +86,7 @@ public class GameManager : MonoBehaviour
 
     private void LateUpdate()
     {
-        FloorUpdate();
+        //FloorUpdate();
     }
 
 
